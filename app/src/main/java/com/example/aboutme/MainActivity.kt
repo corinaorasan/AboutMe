a0f352c5bdf18c8608d6eb4d9883090ccd6bcd1e
corinaorasan171015 <orasan.corina@agilefreaks.com>
2021-02-09 11:49:15 +0200

    Replace setContentView with an instr to create the binding obj

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -7,11 +7,17 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding=DataBindingUtil.setContentView(this, R.layout.activity_main)
         setClickListener()
     }
 
